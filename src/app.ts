dd6707a3bbc97de5c10c4bf07b1ed88f85cf3060
Idba1 <moniraislamself@gmail.com>
2025-07-03 18:44:50 +0600

    fix cors issue

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,24 @@
+// app.ts
 import express from 'express';
+import cors from 'cors';
 import { Request, Response } from 'express';
 import { bookRoutes } from './app/controllers/book.controller';
 import { borrowRoutes } from './app/controllers/borrow.controller';
 
 const app = express();
+
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174'],
+    credentials: true
+}));
+
 app.use(express.json());
 
 app.use('/api/books', bookRoutes);
 app.use('/api/borrow', borrowRoutes);
 
 app.get('/', (req: Request, res: Response) => {
-    res.send(' Library Management API');
+    res.send('Library Management API');
 });
 
 export default app;
-
-
-
